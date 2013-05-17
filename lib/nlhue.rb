093b59a6dd53c168f4efaba013b595c1d3427081
Mike Bourgeous <mike@nitrogenNOSPAMlogic.com>
2013-05-17 02:13:01 -0600

    Added stub methods for logging and benchmarking.
    These methods can be replaced by the application using the library.

--- lib/nlhue.rb
+++ lib/nlhue.rb
@@ -1,5 +1,30 @@
 # Nitrogen Logic's Ruby interface library for the Philips Hue.
-# (C)2012 Mike Bourgeous
+# (C)2013 Mike Bourgeous
+
+# Dummy benchmarking method that may be overridden by library users.
+unless methods.include?(:bench)
+	def bench label, *args, &block
+		yield
+	end
+end
+
+# Logging method that may be overridden by library users.
+unless methods.include?(:log)
+	def log msg
+		puts msg
+	end
+end
+
+# Exception logging method that may be overridden by library users.
+unless methods.include?(:log_e)
+	def log_e e, msg
+		if msg
+			puts "#{msg}: #{e}", e.backtrace
+		else
+			puts e, e.backtrace
+		end
+	end
+end
 
 require_relative 'nlhue/ssdp.rb'
 require_relative 'nlhue/disco.rb'
